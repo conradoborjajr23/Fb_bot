f483055c52921c01010395f569d07a055c37271b
conradoborjajr23 <127187538+conradoborjajr23@users.noreply.github.com>
2024-06-16 06:31:33 +0800

    Update filter.js

--- script/filter.js
+++ script/filter.js
@@ -2,7 +2,7 @@ module.exports.config = {
 		name: "filter",
 		version: "2.0.0",
 		role: 2,
-	credits: "Marjhun Baylon and Miko Mempin",
+	credits: "con",
 		description: "Filter Faceboook User",
 		hasPrefix: false,
 		usages: "",
