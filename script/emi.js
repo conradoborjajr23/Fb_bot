b8ed29a66827506d41b4900b4c88ba087f134c21
Conrado borja <127187538+conradoborjajr23@users.noreply.github.com>
2024-07-10 14:14:27 +0800

    Update emi.js

--- script/emi.js
+++ script/emi.js
@@ -2,48 +2,41 @@ const { get } = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const url = "https://deku-rest-api.replit.app";
-const cacheDir = path.join(__dirname, 'cache');
-const filePath = path.join(cacheDir, 'emi.png');
+let url = "https://joshweb.click";
+let f = path.join(__dirname, 'cache', 'emi.png');
 
 module.exports.config = {
-	name: "emi",
-	version: "1.0.0",
-	role: 0,
-	hasPrefix: false,
-	credits: "con",
-	description: "Generate image in emi",
-	usages: "[prompt]",
-	cooldown: 5,
-	aliases: ["em"]
+    name: "emi",
+    version: "1.0.0",
+    role: 0,
+    hasPrefix: false,
+    credits: "Deku",
+    description: "Generate image in emi",
+    usages: "[prompt]",
+    cooldown: 5,
+    aliases: ["em"]
 };
 
-module.exports.run = async function ({ api, event, args }) {
-	function sendMessage(msg) {
-		api.sendMessage(msg, event.threadID, event.messageID);
-	}
+module.exports["run"] = async function ({ api, event, args }) {
+    function r(msg) {
+        api.sendMessage(msg, event.threadID, event.messageID);
+    }
 
-	if (!args[0]) return sendMessage('Missing prompt!');
-
-	const prompt = args.join(" ");
-	if (!prompt) return sendMessage('Missing prompt!');
-
-	try {
-		const response = await get(`${url}/emi?prompt=${encodeURIComponent(prompt)}`, {
-			responseType: 'arraybuffer'
-		});
-
-		if (response.status === 200) {
-			if (!fs.existsSync(cacheDir)) {
-				fs.mkdirSync(cacheDir, { recursive: true });
-			}
-			fs.writeFileSync(filePath, Buffer.from(response.data, "utf8"));
-			return sendMessage({ attachment: fs.createReadStream(filePath) });
-		} else {
-			return sendMessage("Failed to generate image.");
-		}
-	} catch (error) {
-		console.error('Error:', error);
-		return sendMessage("An error occurred while processing your request.");
-	}
+    const a = args.join(" ");
+    if (!a) return r('Missing prompt!');
+    
+    const cliff = await new Promise(resolve => { api.sendMessage('🔍 | Generating Emi image...', event.threadID, (err, info1) => {
+      resolve(info1);
+     }, event.messageID);
+    });
+    try {
+        const d = (await get(url + '/emi?prompt=' + encodeURIComponent(a), {
+            responseType: 'arraybuffer'
+        })).data;
+        fs.writeFileSync(f, Buffer.from(d, "utf8"));
+        return r({ attachment: fs.createReadStream(f) });
+        fs.unlinkSync(f),event.messageID;    api.unsendMessage(cliff.messageID);
+    } catch (e) {
+        return r(e.message);
+    }
 };
