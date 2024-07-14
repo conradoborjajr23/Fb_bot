bbce92b9afab4872fdc38d5fbc39d23af0085cfd
Conrado borja <127187538+conradoborjajr23@users.noreply.github.com>
2024-07-14 10:10:39 +0800

    Update and rename Ai4.js to Ai6.js

--- script/Ai6.js
+++ script/Ai6.js
@@ -1,7 +1,7 @@
 const { get } = require('axios');
 
 module.exports.config = {
-  name: 'ai4',
+  name: 'ai',
   credits: "con",
   version: '1.0.0',
   role: 0,
@@ -17,7 +17,7 @@ module.exports.run = async function ({ api, event, args }) {
     api.sendMessage(msg, event.threadID, event.messageID);
   }
 
-  const url = "https://hercai.onrender.com/v3/hercai";
+  const url = "https://joshweb.click/api/gpt-4o?q=hi&uid=";
 
   if (!question) return sendMessage("Please provide a question to begin ✨");
 
